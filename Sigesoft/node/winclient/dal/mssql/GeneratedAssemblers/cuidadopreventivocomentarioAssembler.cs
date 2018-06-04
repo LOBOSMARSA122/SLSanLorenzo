b9201d44005df78eb69b2a6feb8e4c2eb7db1aec
amerchan33 <albertomerchanc@hotmail.com>
2018-06-04 16:21:45 -0500

    otros

--- Sigesoft/node/winclient/dal/mssql/GeneratedAssemblers/cuidadopreventivocomentarioAssembler.cs
+++ Sigesoft/node/winclient/dal/mssql/GeneratedAssemblers/cuidadopreventivocomentarioAssembler.cs
@@ -17,38 +17,37 @@ namespace Sigesoft.Node.WinClient.BE
 {
 
     /// <summary>
-    /// Assembler for <see cref="cuidadopreventivo"/> and <see cref="cuidadopreventivoDto"/>.
+    /// Assembler for <see cref="cuidadopreventivocomentario"/> and <see cref="cuidadopreventivocomentarioDto"/>.
     /// </summary>
-    public static partial class cuidadopreventivoAssembler
+    public static partial class cuidadopreventivocomentarioAssembler
     {
         /// <summary>
         /// Invoked when <see cref="ToDTO"/> operation is about to return.
         /// </summary>
-        /// <param name="dto"><see cref="cuidadopreventivoDto"/> converted from <see cref="cuidadopreventivo"/>.</param>
-        static partial void OnDTO(this cuidadopreventivo entity, cuidadopreventivoDto dto);
+        /// <param name="dto"><see cref="cuidadopreventivocomentarioDto"/> converted from <see cref="cuidadopreventivocomentario"/>.</param>
+        static partial void OnDTO(this cuidadopreventivocomentario entity, cuidadopreventivocomentarioDto dto);
 
         /// <summary>
         /// Invoked when <see cref="ToEntity"/> operation is about to return.
         /// </summary>
-        /// <param name="entity"><see cref="cuidadopreventivo"/> converted from <see cref="cuidadopreventivoDto"/>.</param>
-        static partial void OnEntity(this cuidadopreventivoDto dto, cuidadopreventivo entity);
+        /// <param name="entity"><see cref="cuidadopreventivocomentario"/> converted from <see cref="cuidadopreventivocomentarioDto"/>.</param>
+        static partial void OnEntity(this cuidadopreventivocomentarioDto dto, cuidadopreventivocomentario entity);
 
         /// <summary>
-        /// Converts this instance of <see cref="cuidadopreventivoDto"/> to an instance of <see cref="cuidadopreventivo"/>.
+        /// Converts this instance of <see cref="cuidadopreventivocomentarioDto"/> to an instance of <see cref="cuidadopreventivocomentario"/>.
         /// </summary>
-        /// <param name="dto"><see cref="cuidadopreventivoDto"/> to convert.</param>
-        public static cuidadopreventivo ToEntity(this cuidadopreventivoDto dto)
+        /// <param name="dto"><see cref="cuidadopreventivocomentarioDto"/> to convert.</param>
+        public static cuidadopreventivocomentario ToEntity(this cuidadopreventivocomentarioDto dto)
         {
             if (dto == null) return null;
 
-            var entity = new cuidadopreventivo();
+            var entity = new cuidadopreventivocomentario();
 
-            entity.v_CuidadoPreventivoId = dto.v_CuidadoPreventivoId;
+            entity.v_CuidadoPreventivoComentarioId = dto.v_CuidadoPreventivoComentarioId;
             entity.v_PersonId = dto.v_PersonId;
-            entity.d_ServiceDate = dto.d_ServiceDate;
             entity.i_GrupoId = dto.i_GrupoId;
             entity.i_ParametroId = dto.i_ParametroId;
-            entity.i_Valor = dto.i_Valor;
+            entity.v_Comentario = dto.v_Comentario;
             entity.i_IsDeleted = dto.i_IsDeleted;
             entity.i_InsertUserId = dto.i_InsertUserId;
             entity.d_InsertDate = dto.d_InsertDate;
@@ -61,21 +60,20 @@ namespace Sigesoft.Node.WinClient.BE
         }
 
         /// <summary>
-        /// Converts this instance of <see cref="cuidadopreventivo"/> to an instance of <see cref="cuidadopreventivoDto"/>.
+        /// Converts this instance of <see cref="cuidadopreventivocomentario"/> to an instance of <see cref="cuidadopreventivocomentarioDto"/>.
         /// </summary>
-        /// <param name="entity"><see cref="cuidadopreventivo"/> to convert.</param>
-        public static cuidadopreventivoDto ToDTO(this cuidadopreventivo entity)
+        /// <param name="entity"><see cref="cuidadopreventivocomentario"/> to convert.</param>
+        public static cuidadopreventivocomentarioDto ToDTO(this cuidadopreventivocomentario entity)
         {
             if (entity == null) return null;
 
-            var dto = new cuidadopreventivoDto();
+            var dto = new cuidadopreventivocomentarioDto();
 
-            dto.v_CuidadoPreventivoId = entity.v_CuidadoPreventivoId;
+            dto.v_CuidadoPreventivoComentarioId = entity.v_CuidadoPreventivoComentarioId;
             dto.v_PersonId = entity.v_PersonId;
-            dto.d_ServiceDate = entity.d_ServiceDate;
             dto.i_GrupoId = entity.i_GrupoId;
             dto.i_ParametroId = entity.i_ParametroId;
-            dto.i_Valor = entity.i_Valor;
+            dto.v_Comentario = entity.v_Comentario;
             dto.i_IsDeleted = entity.i_IsDeleted;
             dto.i_InsertUserId = entity.i_InsertUserId;
             dto.d_InsertDate = entity.d_InsertDate;
@@ -88,11 +86,11 @@ namespace Sigesoft.Node.WinClient.BE
         }
 
         /// <summary>
-        /// Converts each instance of <see cref="cuidadopreventivoDto"/> to an instance of <see cref="cuidadopreventivo"/>.
+        /// Converts each instance of <see cref="cuidadopreventivocomentarioDto"/> to an instance of <see cref="cuidadopreventivocomentario"/>.
         /// </summary>
         /// <param name="dtos"></param>
         /// <returns></returns>
-        public static List<cuidadopreventivo> ToEntities(this IEnumerable<cuidadopreventivoDto> dtos)
+        public static List<cuidadopreventivocomentario> ToEntities(this IEnumerable<cuidadopreventivocomentarioDto> dtos)
         {
             if (dtos == null) return null;
 
@@ -100,11 +98,11 @@ namespace Sigesoft.Node.WinClient.BE
         }
 
         /// <summary>
-        /// Converts each instance of <see cref="cuidadopreventivo"/> to an instance of <see cref="cuidadopreventivoDto"/>.
+        /// Converts each instance of <see cref="cuidadopreventivocomentario"/> to an instance of <see cref="cuidadopreventivocomentarioDto"/>.
         /// </summary>
         /// <param name="entities"></param>
         /// <returns></returns>
-        public static List<cuidadopreventivoDto> ToDTOs(this IEnumerable<cuidadopreventivo> entities)
+        public static List<cuidadopreventivocomentarioDto> ToDTOs(this IEnumerable<cuidadopreventivocomentario> entities)
         {
             if (entities == null) return null;
 
