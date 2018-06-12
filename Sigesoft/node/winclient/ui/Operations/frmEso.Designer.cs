54a08662dc77e28ea989759799b1716e65f5a8e6
omeganetperu <gerencia@omeganetperu.com>
2018-06-12 18:41:11 -0500

    w

--- Sigesoft/node/winclient/ui/Operations/frmEso.Designer.cs
+++ Sigesoft/node/winclient/ui/Operations/frmEso.Designer.cs
@@ -4760,7 +4760,7 @@
             this.pnlLeftPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlLeftPrincipal.Location = new System.Drawing.Point(0, 0);
             this.pnlLeftPrincipal.Name = "pnlLeftPrincipal";
-            this.pnlLeftPrincipal.Size = new System.Drawing.Size(1358, 736);
+            this.pnlLeftPrincipal.Size = new System.Drawing.Size(1358, 732);
             this.pnlLeftPrincipal.TabIndex = 6;
             // 
             // bgwSaveExamen
@@ -4783,7 +4783,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1350, 753);
+            this.ClientSize = new System.Drawing.Size(1350, 749);
             this.Controls.Add(this.pnlLeftPrincipal);
             this.Name = "frmEso";
             this.ShowIcon = false;
