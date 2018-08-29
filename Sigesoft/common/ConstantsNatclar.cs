01b9354f1c4b2131333dda03e56c37189265d80d
OMEGANETPERU <gerencia@omeganetperu.com>
2018-08-29 16:50:51 -0500

    ConstantsNatclar

--- Sigesoft/common/ConstantsNatclar.cs
+++ Sigesoft/common/ConstantsNatclar.cs
@@ -7,19 +7,47 @@ namespace Sigesoft.Common
 {
     public class ConstantsNatclar
     {
-        #region TIPO_DOCUMENTO
+        #region Datos de Paciente
 
         public const string TIPO_DOCUMENTO_DNI = "1";
         public const string TIPO_DOCUMENTO_PASAPORTE = "2";
         public const string TIPO_DOCUMENTO_OTROS = "5";
         public const string TIPO_DOCUMENTO_EXTRANJERO = "6";
+        public const string TIPO_GENERO_MASCULINO = "M";
+        public const string TIPO_GENERO_FEMENINO = "F";
 
         #endregion
 
-        #region CONSTANTES
+        #region Datos Examen
+        public const string IDESTRUCTURA_ALERGIAS = "1";
+        public const string IDESTRUCTURA_ANALITICA = "2";
+        public const string IDESTRUCTURA_ANTECEDENTES_PATOLOGICOS = "3";
+        public const string IDESTRUCTURA_ANTECEDENTES_QUIRURGICOS = "4";
+        public const string IDESTRUCTURA_AUDIOMETRIA = "5";
+        public const string IDESTRUCTURA_CONSTANTES = "6";
+        public const string IDESTRUCTURA_DATOS_EPISODIO = "7";
+        public const string IDESTRUCTURA_EXAMEN_FISICO = "8";
+        public const string IDESTRUCTURA_EXAMEN_OCULAR_VB = "9";
+        public const string IDESTRUCTURA_EXAMEN_OCULAR_VT = "10";
+        public const string IDESTRUCTURA_HABITOS_TOXICOS = "11";
+        public const string IDESTRUCTURA_HISTORIA_FAMILIAR = "12";
+        public const string IDESTRUCTURA_HISTORIAL_FISIOLOGICO = "13";
+        public const string IDESTRUCTURA_HISTORIA_LABORAL = "14";
+        public const string IDESTRUCTURA_MEDICACIONES = "15";
+        public const string IDESTRUCTURA_TRANSFUSIONES = "16";
+        public const string IDESTRUCTURA_VACUNAS = "17";
+        public const string IDESTRUCTURA_VIGILANCIA_SALUD = "18";
 
-        public const string CONSTANTES_PESO_GRAMOS_6 = "N001-SDSDFSD";
+        public const string IDCENTRO_SAN_LORENZO = "CX35";
 
+        public const string TIPO_EXAMEN_INGRESO = "INGR";
+        public const string TIPO_EXAMEN_PERIODICO = "PERI";
+        public const string TIPO_EXAMEN_RETIRO = "RETI";
+        public const string TIPO_EXAMEN_ESPECIAL = "ESPC";
+        public const string TIPO_EXAMEN_VISITA = "VISI";
+
+        public const string IDESTADO_ACTIVO = "A";
+        public const string IDESTADO_INACTIVO = "I";
         #endregion
        
     }
