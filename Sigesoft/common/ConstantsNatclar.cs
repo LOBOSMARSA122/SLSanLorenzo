f8360e84f2f2d6cd55d954422f84fda474639785
amerchan33 <albertomerchanc@hotmail.com>
2018-08-29 14:52:01 -0500

    otros

---
+++ Sigesoft/common/ConstantsNatclar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sigesoft.Common
+{
+    public class ConstantsNatclar
+    {
+        #region TIPO_DOCUMENTO
+
+        public const string TIPO_DOCUMENTO_DNI = "1";
+        public const string TIPO_DOCUMENTO_PASAPORTE = "2";
+        public const string TIPO_DOCUMENTO_OTROS = "5";
+        public const string TIPO_DOCUMENTO_EXTRANJERO = "6";
+
+        #endregion
+
+        #region CONSTANTES
+
+        public const string CONSTANTES_PESO_GRAMOS_6 = "N001-SDSDFSD";
+
+        #endregion
+       
+    }
+}
---
+++ Sigesoft/node/winclient/BLL/Natclar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sigesoft.Common;
+
+namespace Sigesoft.Node.WinClient.BLL
+{
+    public class Natclar
+    {
+        public void Prueba()
+        {
+            //var x = ConstantsNatclar.TIPO_DOCUMENTO_DNI;
+            //var datosPersona = "";
+
+            //if (dni)
+            //{
+            //   var x = ConstantsNatclar.TIPO_DOCUMENTO_DNI;
+            //}
+            //else if(pasaa))
+            //{
+            //    var x = ConstantsNatclar.TIPO_DOCUMENTO_PASAPORTE;
+            //}
+        
+            //tipodoc = x;
+
+
+        }
+    }
+}
