73ce91bcb6e1a6475c83a10408448346eab89e08
amerchan33 <albertomerchan@hotmail.com>
2018-11-03 15:17:23 -0500

    otros

---
+++ Sigesoft/node/winclient/ui/Hospitalizacion/frmType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sigesoft.Node.WinClient.UI.Hospitalizacion
+{
+    public partial class frmType : Form
+    {
+        public string _conCargoA { get; set; }
+        public frmType()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            _conCargoA = cboPagador.Text;
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
