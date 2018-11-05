259a28eebac480fb4938bdcacbf81eaed89122be
Arnold O. J <armioja_19@omeganetperu.com>
2018-11-05 08:23:10 -0500

    psico web

---
+++ Sigesoft/server/webclientadmin/ui/ExternalUser/FRM031_PSICO.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Sigesoft.Server.WebClientAdmin.UI.ExternalUser {
+    
+    
+    public partial class FRM031_PSICO {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl DivControls;
+    }
+}
