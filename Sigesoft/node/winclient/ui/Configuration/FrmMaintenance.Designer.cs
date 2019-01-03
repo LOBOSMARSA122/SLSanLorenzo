68fb17276e375c26be01eb0cf28d08d9b8ea3f41
Arnold O. J <armioja_19@omeganetperu.com>
2019-01-03 18:37:38 -0500

    empresa - usuarios web

--- Sigesoft/node/winclient/ui/Configuration/FrmMaintenance.Designer.cs
+++ Sigesoft/node/winclient/ui/Configuration/FrmMaintenance.Designer.cs
@@ -202,6 +202,8 @@
             // 
             // cboEmpresa
             // 
+            this.cboEmpresa.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cboEmpresa.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cboEmpresa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboEmpresa.FormattingEnabled = true;
