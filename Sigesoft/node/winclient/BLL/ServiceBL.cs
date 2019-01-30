89bd128e35e225a19d5fce3cd71e9d806ecb1e8a
Arnold O. J <armioja_19@omeganetperu.com>
2019-01-30 16:06:32 -0500

    liqu

--- Sigesoft/node/winclient/BLL/ServiceBL.cs
+++ Sigesoft/node/winclient/BLL/ServiceBL.cs
@@ -32009,10 +32009,9 @@ namespace Sigesoft.Node.WinClient.BLL
                                 //&& A.d_ServiceDate > pdatBeginDate && A.d_ServiceDate < pdatEndDate && C.d_Birthdate != null && A.i_IsFac != 2 && A.v_NroLiquidacion == null || A.v_NroLiquidacion == ""
                                 //&& A1.i_CalendarStatusId != 4
 
-                                where A.i_IsDeleted == 0 && A1.i_CalendarStatusId != 4 
-                                && A.d_ServiceDate > pdatBeginDate && A.d_ServiceDate < pdatEndDate && C.d_Birthdate != null && A.i_IsFac != 2
-                                && A.v_NroLiquidacion == null
-                                //&& A.i_ServiceStatusId == 3
+                                where A.i_IsDeleted == 0 && A1.i_CalendarStatusId != 4
+                                && (A.d_ServiceDate > pdatBeginDate && A.d_ServiceDate < pdatEndDate) && C.d_Birthdate != null && A.i_IsFac != 2 && (A.v_NroLiquidacion == null || A.v_NroLiquidacion == "")
+                                //&& A.i_ServiceStatusId == 3  
                                 select new Liquidacion
                                 {
                                     v_ServiceId = A.v_ServiceId,
@@ -32153,9 +32152,8 @@ namespace Sigesoft.Node.WinClient.BLL
                                                                     equals new { a = H.i_ParameterId, b = H.i_GroupId } into J5_join
                                 from H in J5_join.DefaultIfEmpty()
 
-                                where A.i_IsDeleted == 0 
-                                && A.d_ServiceDate > pdatBeginDate && A.d_ServiceDate < pdatEndDate && C.d_Birthdate != null && A.i_IsFac != 2
-                                && A1.i_CalendarStatusId != 4
+                                where A.i_IsDeleted == 0 && A1.i_CalendarStatusId != 4
+                                && (A.d_ServiceDate > pdatBeginDate && A.d_ServiceDate < pdatEndDate) && C.d_Birthdate != null && A.i_IsFac != 2
                                 select new Liquidacion
                                 {
                                     v_ServiceId = A.v_ServiceId,
